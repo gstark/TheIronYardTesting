ca7f9190f188f7afbaffa4b42238417bf2e8de7a
Gavin Stark <gavin@gstark.com>
2015-03-18 19:19:02 -0400

    Adds action -- changes message -- now complaining that we are missing a view template

--- book_store/app/controllers/articles_controller.rb
+++ book_store/app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  # GET /search
+  def search
+  end
+
   # GET /articles/1
   # GET /articles/1.json
   def show
