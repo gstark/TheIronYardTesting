e6595cce1472f715b6f90a437c86736fc9034115
Gavin Stark <gavin@gstark.com>
2015-03-18 19:08:59 -0400

    Improves test failure message

--- book_store/test/models/article_test.rb
+++ book_store/test/models/article_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ArticleTest < ActiveSupport::TestCase
   test "should not save article without title" do
     article = Article.new
-    assert_not article.save
+    assert_not article.save, "Incorrectly saved an article without a title"
   end
 end
 
